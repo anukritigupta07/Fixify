06802ceecca4c6ccb1c899b9885e57fefc1e9797
Utkarsh Dwivedi <dutkarsh666@gmail.com>
2025-09-12 18:33:59 +0530

    initial commit

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -11,8 +11,13 @@ const mapsRoutes = require('./routes/maps.routes')
 const bookingRoutes = require("./routes/booking.route");
 
 connecToDb();
-
-app.use(cors())
+app.use(express.static('public'));
+app.use(cors({
+  origin: ['https://fixify-1-vn2l.onrender.com', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
  app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
